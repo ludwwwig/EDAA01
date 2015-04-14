146b0a7e30f9402ba3eb53e49572cb9d888c447f
anle95 <tpi14ale@student.lu.se>
2015-04-14 18:46:15 +0200

    Otroligt nog: Lab 3 klar

--- Workspace/edaa01-workspace/lab3/src/fractal/FractalApplication.java
+++ Workspace/edaa01-workspace/lab3/src/fractal/FractalApplication.java
@@ -2,12 +2,13 @@ package fractal;
 
 import koch.Koch;
 import mountain.Mountain;
+import mountain.Point;
 
 public class FractalApplication {
 	public static void main(String[] args) {
 		Fractal[] fractals = new Fractal[2];
 		fractals[0] = new Koch(300);
-		fractals[1] = new Mountain(0);
+		fractals[1] = new Mountain(new Point(230, 50), new Point(50, 380), new Point(500, 475), 30);
 	    new FractalView(fractals, "Fraktaler");
 	}
 
--- Workspace/edaa01-workspace/lab3/src/mountain/Mountain.java
+++ Workspace/edaa01-workspace/lab3/src/mountain/Mountain.java
@@ -1,12 +1,19 @@
 package mountain;
 
 import fractal.*;
+import java.util.LinkedList;
 
 public class Mountain extends Fractal {
+	private Point a, b, c;
 	private double dev;
+	private LinkedList<Side> sideList;
 	
-	public Mountain(double deviation) {
+	public Mountain(Point a, Point b, Point c, double deviation) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
 		dev = deviation;
+		sideList = new LinkedList<Side>();
 	}
 	
 	public String getTitle() {
@@ -14,44 +21,72 @@ public class Mountain extends Fractal {
 	}
 	
 	public void draw(TurtleGraphics turtle) {
-		fractalTriangle(turtle, order, new Point(230, 50), new Point(100, 350), new Point(500, 375));
+		fractalTriangle(turtle, order, a, b, c, dev);
 	}
 	
-	private static double randFunc(double dev) {
-        double t = dev * Math.sqrt(-2 * Math.log(Math.random()));
-        if (Math.random() < 0.5) {
-        	t = -t; 
-        }
-        return t; 
-    }
-	
-	private void fractalTriangle(TurtleGraphics turtle, int order, Point p1, Point p2, Point p3) {
+	private void fractalTriangle(TurtleGraphics turtle, int order, Point a, Point b, Point c, Double dev) {
 		if(order == 0) {
-			drawTriangle(turtle, p1, p2, p3);
+			drawTriangle(turtle, a, b, c);
 		} else {
-			double deviation = dev/(Math.pow(2, order));
-			double a1 = randFunc(deviation);
-			double a2 = randFunc(deviation);
-			double b1 = randFunc(deviation);
-			double b2 = randFunc(deviation);
-			double c1 = randFunc(deviation);
-			double c2 = randFunc(deviation);
+			double aDev = RandomUtilities.randFunc(dev);
+			double bDev = RandomUtilities.randFunc(dev);
+			double cDev = RandomUtilities.randFunc(dev);
 			
-			Point p4 = new Point(a1 + (p1.getX()+p2.getX())/2, a2 + (p1.getY()+p2.getY())/2);
-			Point p5 = new Point(b1 + (p1.getX()+p3.getX())/2, b2 + (p1.getY()+p3.getY())/2);
-			Point p6 = new Point(c1 + (p2.getX()+p3.getX())/2, c2 + (p2.getY()+p3.getY())/2);
+			Point abMid = new Point((a.getX()+b.getX())/2, aDev + (a.getY()+b.getY())/2);
+			Point acMid = new Point((a.getX()+c.getX())/2, bDev + (a.getY()+c.getY())/2);
+			Point bcMid = new Point((b.getX()+c.getX())/2, cDev + (b.getY()+c.getY())/2);
+			Side abSide = new Side(a, b, abMid);
+			Side acSide = new Side(a, c, acMid);
+			Side bcSide = new Side(b, c, bcMid);
 			
-			fractalTriangle(turtle, order-1, p1, p4, p5);
-			fractalTriangle(turtle, order-1, p4, p2, p6);
-			fractalTriangle(turtle, order-1, p5, p6, p3);
-			fractalTriangle(turtle, order-1, p4, p5, p6);
+			Point check; 
+			if((check = findMid(abSide)) != null) {
+				abMid = check;
+				sideList.remove(check);
+			} else
+				sideList.add(abSide);
+			if ((check = findMid(acSide)) != null) {
+				acMid = check;
+				sideList.remove(check);
+			} else
+				sideList.add(acSide);
+			if ((check = findMid(bcSide)) != null) {
+				bcMid = check;
+				sideList.remove(check);
+			} else 
+				sideList.add(bcSide);
+			
+			fractalTriangle(turtle, order-1, a, abMid, acMid, dev/2);
+			fractalTriangle(turtle, order-1, abMid, b, bcMid, dev/2);
+			fractalTriangle(turtle, order-1, acMid, bcMid, c, dev/2);
+			fractalTriangle(turtle, order-1, abMid, acMid, bcMid, dev/2);
 		}
 	}
-
+	
 	private void drawTriangle(TurtleGraphics turtle, Point p1, Point p2, Point p3){
 		turtle.moveTo(p1.getX(), p1.getY());
 		turtle.forwardTo(p2.getX(), p2.getY());
 		turtle.forwardTo(p3.getX(), p3.getY());
 		turtle.forwardTo(p1.getX(), p1.getY());
+	}
+	
+	private Point findMid(Side s) {
+		for(Side side : sideList) {
+			if(side.equals(s))
+				return side.m;
+		}
+		return null;
+	}
+	
+	private class Side {
+		Point a, b, m;
+		public Side(Point p1, Point p2, Point mid) {
+			a = p1;
+			b = p2;
+			m = mid;
+		}
+		public boolean equals(Side s) {
+			return ((a == s.a && b == s.b) || (b == s.a && a == s.b));
+		}
 	}
 }
