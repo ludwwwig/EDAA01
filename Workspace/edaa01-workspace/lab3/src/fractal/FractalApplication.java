51af8fa03357c23a4e20b5047c7884fb0aa869fb
ludwwwig <ludwig.hollmann@gmail.com>
2015-04-14 17:28:46 +0200

    Labb 3 förhoppningsvis klar...

--- Workspace/edaa01-workspace/lab3/src/fractal/FractalApplication.java
+++ Workspace/edaa01-workspace/lab3/src/fractal/FractalApplication.java
@@ -6,8 +6,8 @@ import mountain.Point;
 public class FractalApplication {
 	public static void main(String[] args) {
 		Fractal[] fractals = new Fractal[2];
-		fractals[0] = new Koch(300);
-	    fractals[1] = new Mountain(new Point(30,30) , new Point(500,500), new Point(100,550));
+		fractals[1] = new Koch(300);
+	    fractals[0] = new Mountain(new Point(30,30) , new Point(500,500), new Point(100,550), 100);
 		new FractalView(fractals, "Fraktaler");
 	    
 	}
