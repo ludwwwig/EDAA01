e1ff78d6a72c8ca1084e829373496107e73e2a43
anle95 <tpi14ale@student.lu.se>
2015-07-26 22:47:47 +0200

    Rensning igen, alla laborationer klara

--- ProjectEulerScala/src/main.scala
+++
@@ -1,8 +0,0 @@
-object main {
-  def main(args: Array[String]) {
-    var tree = new Tree[Integer](new Node(1))
-    tree.addLevel()
-    tree.addLevel()
-    tree.traverse(println)
-  }
-}
--- Workspace/edaa01-workspace/lab6/src/phonebook/Main.java
+++
@@ -1,9 +0,0 @@
-package phonebook;
-
-public class Main {
-	public static void main(String[] args) {	
-		PhoneBook pb = new PhoneBook();
-		new PhoneBookGUI(pb);
-	}
-
-}
